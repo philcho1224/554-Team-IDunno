a47bca1389a5d2d91c839b34bc58aff4fad4e518
Philip Cho <pcho2@stevens.edu>
2020-05-06 02:25:38 -0400

    added some stuff to app.js and made components folder

--- ronaapp/src/App.js
+++ ronaapp/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path = '/' exact component = {Landing}/>
+          <Route exact path = '/marketplace' exact component = {Marketplace}/>
+          <Route exact path = '/login' exact component = {Login}/>
+          <Route exact path = '/register' exact component = {Register}/>
+          <Route exact path = '/info' exact component = {Info}/>
+        </Switch>
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
