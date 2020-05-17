bbdb074934a5518c58fd901d6f041c538f7390e9
Philip Cho <pcho2@stevens.edu>
2020-05-17 17:56:47 -0400

    added algolia

--- ronaapp/src/components/marketplace.js
+++ ronaapp/src/components/marketplace.js
@@ -1,31 +1,33 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { getAllItems, addItem, getUser, deleteItem, getUserItems } from '../firebase/FirebaseFunc';
 import { AuthContext } from '../firebase/Auth';
-import { Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, IconButton, Typography, Grid, Box, FormControl, FormHelperText } from '@material-ui/core';
-import { Button, InputLabel, Input, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
-import Container from '@material-ui/core/Container';
-
-import * as google from 'google-parser'
-// const imageSearch = require('image-search-google');
+import {Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, IconButton, Typography, Grid} from '@material-ui/core';
+import {Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from  '@material-ui/core';
+import { InstantSearch, SearchBox, Hits, Highlight, connectHits } from 'react-instantsearch-dom';
+import * as algoliasearch from 'algoliasearch';
 import * as imageSearch from 'image-search-google';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import { Fab } from '@material-ui/core/';
-import AddIcon from '@material-ui/icons/Add';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// const axios = require('axios');
-// const {Storage} = require('@google-cloud/storage');
-import * as Storage from '@google-cloud/storage';
-import * as storage from '@google-cloud/storage';
 import * as axios from 'axios';
 import { Redirect } from 'react-router-dom';
+
+
+const client = algoliasearch("NW00BIZJ9O", "73ab8c260b27b919c60e626eaad30649");
+
+
 // import * as  Scraper from 'images-scraper';
+
+
+
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
-  cardContainer: {
+  cardContainer:{
     display: "flex"
   },
   media: {
@@ -44,16 +46,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// async function uploadFile(filename) {
-//   // await axios.post(
-//   //   'https://storage.googleapis.com/media/storage/v1/b/ronaapp_market_items/o',
-//   //   {
-//   //     object : filename
-//   //   }
-//   // );
-//   let image = await axios.get('https://storage.googleapis.com/storage/v1/b/ronaapp_market_items/o/dog.jpeg');
-//   console.log(image);
-// }
 
 
 
@@ -66,7 +58,7 @@ const ShowItems = () => {
   };
 
   const { currentUser } = useContext(AuthContext);
-  console.log("CurrentUser UID:", currentUser.uid);
+  console.log("CurrentUser UID:" , currentUser.uid);
   const [marketItems, setMarketItems] = useState(undefined);
   const [usrInfo, setUsrInfo] = useState(undefined);
   const [redirect, setRedirect] = useState(false);
@@ -79,66 +71,134 @@ const ShowItems = () => {
   const handleClose = () => {
     setOpen(false);
   };
-  async function deleteIt() {
-    await deleteItem("hello", "2rdsnxrzmS47UJEduoid");
+  async function deleteIt(){
+    await deleteItem("hello","2rdsnxrzmS47UJEduoid");
   }
-  async function userItems() {
+  async function userItems(){
     await getUserItems(usrInfo.email);
   }
-  async function getImage(image) {
+  async function getImage(image){
     const client = new imageSearch('006652411016456664611:esbhjblxvz6', 'AIzaSyANzy7uYHb-BEzcBkjxGVQLKWfpf3f5eEo');
-    const options = { page: 1 };
+    const options = {page:1};
     let imageResult = await client.search(image, options);
     let sliceIndex = imageResult[0].url.search(".jpeg");
-    let imageString = imageResult[0].url.substr(0, sliceIndex + 5);
+    let imageString = imageResult[0].url.substr(0,sliceIndex + 5);
     return imageString;
 
   }
   useEffect(() => {
-    async function getUserInfo(uid) {
+    async function getUserInfo(uid){
       let userInfo;
-      try {
+      try{
         userInfo = await getUser(uid);
-        console.log("Async Func", userInfo);
+        console.log("Async Func" ,userInfo);
         setUsrInfo(userInfo);
       }
-      catch (e) {
+      catch(e){
         console.log(e);
       }
     };
 
-    async function getItems() {
+    async function getItems(){
       let allItems;
-      try {
+      try{
         allItems = await getAllItems();
         console.log(allItems);
         setMarketItems(allItems);
       }
-      catch (e) {
+      catch(e){
         console.log(e);
       }
 
     };
     getItems();
     getUserInfo(currentUser.uid);
-    console.log("UsrInfo In Effect", usrInfo);
+    console.log("UsrInfo In Effect" ,usrInfo);
   }, [currentUser.uid]
   );
-  console.log("UsrInfo", usrInfo);
-  if (usrInfo) {
+  console.log("UsrInfo",usrInfo);
+  if(usrInfo){
     console.log("This is user info", usrInfo);
   }
-  if (redirect === true) {
+  if(redirect === true){
     return <Redirect to="/market" />;
   }
   console.log(marketItems);
-
+  //***************WORK IN PROGRESS*********************
+  const Hit = ({hit}) =>{
+    const classes = useStyles();
+    return(
+      <Grid item xs={4}>
+          <Card className={classes.root}>
+        <CardHeader
+          title= {hit.name}
+          subheader= {hit.user}
+        />
+        <CardMedia
+          className={classes.media}
+          image={hit.image}
+          title={hit.name}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {hit.description}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Trade Items:
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs = {4}>
+              {hit.tradeitems[0]}
+            </Grid>
+            <Grid item xs = {4}>
+              {hit.tradeitems[1]}
+            </Grid>
+            <Grid item xs = {4}>
+              {hit.tradeitems[2]}
+            </Grid>
+          </Grid>
+  
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Contact Info:</Typography>
+            <Typography paragraph>
+              {hit.email}
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
+  
+    )
+  }
+  
+  const Content = () =>
+    <div className = "content">
+    {/* <Grid container spacing={4}> */}
+    {/* <div> */}
+      <Hits hitComponent = {Hit}/>
+    {/* </Grid> */}
+    </div>
+// ****************************************
   const handleNewPost = async (event) => {
-
+    
     console.log("EVENT: ", event);
     event.preventDefault();
 
-    let { name, description, tradeitem1, tradeitem2, tradeitem3 } = event.target.elements;
+    let {name, description, tradeitem1, tradeitem2, tradeitem3} = event.target.elements;
     let imageString = await getImage(name.value);
 
     let itemObject = {
@@ -152,11 +212,11 @@ const ShowItems = () => {
     }
     console.log("ItemObject");
     console.log(itemObject);
-    try {
+    try{
       await addItem(itemObject);
       // await uploadFile(image);
     }
-    catch (error) {
+    catch(error){
       console.log(error);
     }
     handleClose();
@@ -166,155 +226,165 @@ const ShowItems = () => {
   let cards = null;
   cards = marketItems && marketItems.map((value, index) => {
     console.log("Value: ", value);
-    return (
+    return(
       <Grid item xs={4}>
         <Card className={classes.root}>
-          <CardHeader
-            title={value.name}
-            subheader={value.user}
-          />
-          <CardMedia
-            className={classes.media}
-            image={value.image}
-            title={value.name}
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {value.description}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Trade Items:
+      <CardHeader
+        title= {value.name}
+        subheader= {value.user}
+      />
+      <CardMedia
+        className={classes.media}
+        image={value.image}
+        title={value.name}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {value.description}
         </Typography>
-            <Grid container spacing={4}>
-              <Grid item xs={4}>
-                {value.tradeitems[0]}
-              </Grid>
-              <Grid item xs={4}>
-                {value.tradeitems[1]}
-              </Grid>
-              <Grid item xs={4}>
-                {value.tradeitems[2]}
-              </Grid>
-            </Grid>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Trade Items:
+        </Typography>
+        <Grid container spacing={4}>
+          <Grid item xs = {4}>
+            {value.tradeitems[0]}
+          </Grid>
+          <Grid item xs = {4}>
+            {value.tradeitems[1]}
+          </Grid>
+          <Grid item xs = {4}>
+            {value.tradeitems[2]}
+          </Grid>
+        </Grid>
 
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton
-              className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded,
-              })}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography paragraph>Contact Info:</Typography>
-              <Typography paragraph>
-                {value.email}
-              </Typography>
-            </CardContent>
-          </Collapse>
-        </Card>
-        {/* <Card variant = "outlined" key = {index}>
-        {Object.keys(value).map(function (key) {
-            console.log("Key", key);
-            let item = value[key];
-            return <p>{item}</p>
-
-          })};
-      </Card> */}
-      </Grid>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Contact Info:</Typography>
+          <Typography paragraph>
+            {value.email}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  </Grid>
     )
   });
-
-  return (
-    <div>
-      <Container component="main" maxWidth="lg" >
-        <Box mt={3}>
-          <Typography variant="h3">Marketplace</Typography>
-        </Box>
-
-        <Fab variant="extended" color="primary" onClick={handleClickOpen}>
-          <AddIcon />
-          Create Post
-        </Fab>
-
-
+    return (
+      <div>
+        <h1>Marketplace</h1>
+        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          Create New Post
+        </Button>
+        <InstantSearch
+          // ...
+          searchClient={client}
+          indexName="marketitems"
+        >
+          <header>
+            <SearchBox translations = {{placeholder:'Search Box'}}/>
+          </header>
+          <main>
+            <Content/>
+          </main>
+ 
+        </InstantSearch>
         <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">New Post</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Provide the name and description of the item along with items you are seeking! (Someone make sure i have this the right way or is it the other way around)
-            </DialogContentText>
-            <form onSubmit={handleNewPost}>
-
-              <FormControl fullWidth>
-                <InputLabel htmlFor="name">Name of Item</InputLabel>
-                <Input id="name" name="name" type="text" required />
-              </FormControl>
-
-              <Box mt={4}>
-                <FormControl fullWidth>
-                  <InputLabel htmlFor="description">Description of Item</InputLabel>
-                  <Input id="description" name="description" type="text" required />
-                </FormControl>
-              </Box>
-
-
-              <Box mt={4}>
-                <Grid container fullWidth>
-                  <Grid item xs>
-                    <FormControl fullWidth>
-                      <InputLabel htmlFor="tradeitem1">Trade Item 1</InputLabel>
-                      <Input id="tradeitem1" name="tradeitem1" type="text" required />
-                      <FormHelperText id="tradeitem1-helper-text">Ex. Toilet Paper</FormHelperText>
-                    </FormControl>
-                  </Grid>
-                  <Grid item xs  class="Middle_item" >
-                    <FormControl fullWidth>
-                      <InputLabel htmlFor="tradeitem2">Trade Item 2</InputLabel>
-                      <Input id="tradeitem2" name="tradeitem2" type="text" required />
-                    </FormControl>
-                  </Grid>
-                  <Grid item xs>
-                    <FormControl fullWidth>
-                      <InputLabel htmlFor="tradeitem3">Trade Item 3</InputLabel>
-                      <Input id="tradeitem3" name="tradeitem3" type="text" required />
-                    </FormControl>
-                  </Grid>
-                </Grid>
-              </Box>
-
-              <Button onClick={handleClose} color="primary">Cancel</Button>
-              <Button type="submit">Create Post</Button>
-
-              <DialogActions>
-                <Button onClick={handleClose} color="primary">Cancel</Button>
-                <Button type="submit" color="primary">Create Post</Button>
-              </DialogActions>
-            </form>
-          </DialogContent>
-
+        <DialogTitle id="form-dialog-title">New Post</DialogTitle>
+        <form onSubmit = {handleNewPost}>
+          <div className = "form-group">
+            <label>
+              Name:
+              <input
+                className = "form-control"
+                name = "name"
+                id = "name"
+                type = "name"
+                placeholder = "Name of Item"
+                required
+              />
+            </label>
+          </div>
+          <div className = "form-group">
+            <label>
+              Description:
+              <input
+                className = "form-control"
+                name = "description"
+                id = "description"
+                type = "description"
+                placeholder = "Description of item"
+                required
+              />
+            </label>
+          </div>
+          <div className = "form-group">
+                  <label>
+                      Trade Item 1:
+                      <input
+                      className="form-control"
+                      name="tradeitem1"
+                      type="tradeitem1"
+                      placeholder="Ex: Toilet Paper"
+  
+                      />
+                  </label>
+              </div>
+              <div className = "form-group">
+                  <label>
+                      Trade Item 2:
+                      <input
+                      className="form-control"
+                      name="tradeitem2"
+                      type="tradeitem2"
+                      placeholder="Ex: Toilet Paper"
+                      />
+                  </label>
+              </div>
+              <div className = "form-group">
+                  <label>
+                      Trade Item 3:
+                      <input
+                      className="form-control"
+                      name="tradeitem3"
+                      type="tradeitem3"
+                      placeholder="Ex: Toilet Paper"
+                      />
+                  </label>
+              </div>
+              <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+          <Button type="submit">Create Post</Button>
+      </form>
         </Dialog>
 
-        <Box mt={6}>
-          <Grid container spacing={4}>
-            {cards}
-          </Grid>
-        </Box>
 
-        <Box mt={8}>
-          <Button onClick={deleteIt}>Delete Item</Button>
-          <Button onClick={userItems}>Show User Item</Button>
-        </Box>
-
-      </Container>
-    </div>
-  );
+        {/* <Grid container spacing={4}>
+          {cards}
+          
+        </Grid> */}
+  
+       
+      {/* <button onClick={deleteIt}>Delete Item</button>
+      <button onClick={userItems}>Show User Item</button> */}
+      </div>
+    );
+  
+ 
+  
 
 }
 
